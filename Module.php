6c6c2ce824c3dbc6889c271c2e775528a6e5adb5
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-01-01 00:00:00 +0000

    Replaced AbstractModule by a trait.

--- Module.php
+++ Module.php
@@ -22,4 +22,8 @@ use Omeka\Module\AbstractModule;
  */
 class Module extends AbstractModule
 {
+    public function getConfig()
+    {
+        return require __DIR__ . '/config/module.config.php';
+    }
 }
