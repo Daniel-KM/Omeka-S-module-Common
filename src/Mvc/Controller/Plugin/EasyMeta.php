9e93d86d6cb0e38bac489a33df975d33832cb23c
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-02-05 00:00:00 +0000

    Added doc blocks to EasyMeta helpers.

--- src/Mvc/Controller/Plugin/EasyMeta.php
+++ src/Mvc/Controller/Plugin/EasyMeta.php
@@ -16,11 +16,50 @@ class EasyMeta extends AbstractPlugin
         $this->easyMeta = $easyMeta;
     }
 
+    /**
+     * Get EasyMeta.
+     */
     public function __invoke(): \Common\Stdlib\EasyMeta
     {
         return $this->easyMeta;
     }
 
+    /**
+     * Proxy to EasyMeta.
+     *
+     * @see \Common\Stdlib\EasyMeta
+     *
+     * @method string|null entityClass($name)
+     * @method string|null resourceName($name)
+     * @method string|null resourceLabel($name)
+     * @method string|null resourceLabelPlural($name)
+     * @method string|null dataTypeName(?string $dataType)
+     * @method array dataTypeNames($dataTypes = null)
+     * @method string|null dataTypeMain(?string $dataType)
+     * @method string|null dataTypeMainCustomVocab(?string $dataType)
+     * @method int|null propertyId($termOrId)
+     * @method array propertyIds($termsOrIds = null)
+     * @method string|null propertyTerm($termOrId)
+     * @method array propertyTerms($termsOrIds = null)
+     * @method string|null propertyLabel($termOrId)
+     * @method array propertyLabels($termsOrIds = null)
+     * @method int|null resourceClassId($termOrId)
+     * @method array resourceClassIds($termsOrIds = null)
+     * @method string|null resourceClassTerm($termOrId)
+     * @method array resourceClassTerms($termsOrIds = null)
+     * @method string|null resourceClassLabel($termOrId)
+     * @method array resourceClassLabels($termsOrIds = null)
+     * @method int|null resourceTemplateId($labelOrId)
+     * @method array resourceTemplateIds($labelsOrIds = null)
+     * @method string|null resourceTemplateLabel($labelOrId)
+     * @method array resourceTemplateLabels($labelsOrIds = null)
+     * @method int|null vocabularyId($prefixOrUriOrId)
+     * @method array vocabularyIds($prefixesOrUrisOrIds = null)
+     * @method string|null vocabularyPrefix($prefixOrUriOrId)
+     * @method array vocabularyPrefixes($prefixesOrUrisOrIds = null)
+     * @method string|null vocabularyUri($prefixOrUriOrId)
+     * @method array vocabularyUrisByPrefixes($prefixesOrUrisOrIds = null)
+     */
     public function __call(string $name , array $arguments)
     {
         return $this->easyMeta->$name(...$arguments);
