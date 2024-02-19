7c1f1ccf35ddac13554a101aad5963d2e526d670
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-02-19 00:00:00 +0000

    Added indexes in tables fulltext_search, media and value.

--- Module.php
+++ Module.php
@@ -47,36 +47,107 @@ class Module extends AbstractModule
      */
     protected function fixIndexes(ServiceLocatorInterface $services)
     {
-        // Early fix media_type index.
+        // Early fix media_type index and other common indexes.
         // See migration 20240219000000_AddIndexMediaType.
         $connection = $services->get('Omeka\Connection');
-        $messenger = $services->get('ControllerPluginManager')->get('messenger');
+
         $sql = <<<'SQL'
+ALTER TABLE `asset`
+CHANGE `media_type` `media_type` varchar(190) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `name`,
+CHANGE `extension` `extension` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `storage_id`;
+
+ALTER TABLE `job`
+CHANGE `pid` `pid` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `owner_id`,
+CHANGE `status` `status` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `pid`,
+CHANGE `class` `class` varchar(190) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `status`;
+
 ALTER TABLE `media`
 CHANGE `ingester` `ingester` varchar(190) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `item_id`,
 CHANGE `renderer` `renderer` varchar(190) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `ingester`,
 CHANGE `media_type` `media_type` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `source`,
 CHANGE `extension` `extension` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `storage_id`;
+
+ALTER TABLE `module`
+CHANGE `version` `version` varchar(190) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `is_active`;
+
+ALTER TABLE `resource`
+CHANGE `resource_type` `resource_type` varchar(190) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `modified`;
+
+ALTER TABLE `resource_template_property`
+CHANGE `default_lang` `default_lang` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `is_private`;
+
+ALTER TABLE `value`
+CHANGE `type` `type` varchar(190) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `value_resource_id`,
+CHANGE `lang` `lang` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `type`;
+
 SQL;
         $connection->executeStatement($sql);
+
+        if (version_compare(\Omeka\Module::VERSION, '4.1', '>=')) {
+            $sql = <<<'SQL'
+ALTER TABLE `site_page`
+CHANGE `layout` `layout` varchar(190) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `modified`;
+SQL;
+            try {
+                $connection->executeStatement($sql);
+            } catch (\Exception $e) {
+                // Omekas S version is not 4.1
+            }
+        }
+
+        $hasNewIndex = false;
+
+        try {
+            $connection->executeStatement('ALTER TABLE `fulltext_search` ADD INDEX `is_public` (`is_public`);');
+            $hasNewIndex = true;
+        } catch (\Exception $e) {
+            // Index exists.
+        }
+
         try {
             $connection->executeStatement('ALTER TABLE `media` ADD INDEX `ingester` (`ingester`);');
+            $hasNewIndex = true;
         } catch (\Exception $e) {
             // Index exists.
         }
         try {
             $connection->executeStatement('ALTER TABLE `media` ADD INDEX `renderer` (`renderer`);');
+            $hasNewIndex = true;
         } catch (\Exception $e) {
             // Index exists.
         }
         try {
             $connection->executeStatement('ALTER TABLE `media` ADD INDEX `media_type` (`media_type`);');
-            $message = new \Common\Stdlib\PsrMessage(
-                'An index has been added to table media to improve performance.' // @translate
-            );
-            $messenger->addSuccess($message);
+            $hasNewIndex = true;
         } catch (\Exception $e) {
             // Index exists.
+        }
+        try {
+            $connection->executeStatement('ALTER TABLE `media` ADD INDEX `extension` (`extension`);');
+            $hasNewIndex = true;
+        } catch (\Exception $e) {
+            // Index exists.
+        }
+
+        try {
+            $connection->executeStatement('ALTER TABLE `value` ADD INDEX `type` (`type`);');
+            $hasNewIndex = true;
+        } catch (\Exception $e) {
+            // Index exists.
+        }
+        try {
+            $connection->executeStatement('ALTER TABLE `value` ADD INDEX `lang` (`lang`);');
+            $hasNewIndex = true;
+        } catch (\Exception $e) {
+            // Index exists.
+        }
+
+        if ($hasNewIndex) {
+            $message = new \Common\Stdlib\PsrMessage(
+                'Some indexes were added to tables to improve performance.' // @translate
+            );
+            $messenger = $services->get('ControllerPluginManager')->get('messenger');
+            $messenger->addSuccess($message);
         }
     }
 }
