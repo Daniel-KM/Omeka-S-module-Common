0b7a9aa9ed0bb34ebdc92ddfd70f0dd059dffc8f
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-06-09 00:00:00 +0000

    Allowed to remove page and sort arguments for form element UrlQuery.

--- src/Form/Element/UrlQuery.php
+++ src/Form/Element/UrlQuery.php
@@ -7,6 +7,20 @@ use Laminas\InputFilter\InputProviderInterface;
 
 class UrlQuery extends Text implements InputProviderInterface
 {
+    /**
+     * @var bool
+     */
+    protected $removeArgumentsPageAndSort = false;
+
+    public function setOptions($options)
+    {
+        parent::setOptions($options);
+        if (array_key_exists('remove_arguments_page_and_sort', $this->options)) {
+            $this->setRemoveArgumentsPageAndSort($this->options['remove_arguments_page_and_sort']);
+        }
+        return $this;
+    }
+
     public function setValue($value)
     {
         $this->value = $this->arrayToQuery($value);
@@ -42,8 +56,52 @@ class UrlQuery extends Text implements InputProviderInterface
         if (is_array($string)) {
             return $string;
         }
+
+        if (!strlen($string)) {
+            return [];
+        }
+
         $query = [];
         parse_str(ltrim((string) $string, "? \t\n\r\0\x0B"), $query);
+
+        if ($this->removeArgumentsPageAndSort) {
+            $query = $this->removeArgumentsPageAndSort($query);
+        }
+
         return $query;
+    }
+
+    /**
+     * Remove arguments page and sort.
+     */
+    public function removeArgumentsPageAndSort(array $query): array
+    {
+        unset(
+            $query['page'],
+            $query['per_page'],
+            $query['offset'],
+            $query['limit'],
+            $query['sort_by'],
+            $query['sort_order'],
+            $query['sort_by_default'],
+            $query['sort_order_default'],
+            $query['submit'],
+            // Not for standard search, but common.
+            $query['sort'],
+            $query['order'],
+            $query['order_by']
+        );
+        return $query;
+    }
+
+    public function setRemoveArgumentsPageAndSort($removeArgumentsPageAndSort): self
+    {
+        $this->removeArgumentsPageAndSort = (bool) $removeArgumentsPageAndSort;
+        return $this;
+    }
+
+    public function getRemoveArgumentsPageAndSort(): bool
+    {
+        return $this->removeArgumentsPageAndSort;
     }
 }
