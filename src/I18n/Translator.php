c84cbbb46fc68cf4a912ad2b48679ed5dd1e1ed0
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-07-28 00:00:00 +0000

    Fixed translation of plurals in translatePlural().

--- src/I18n/Translator.php
+++ src/I18n/Translator.php
@@ -49,4 +49,45 @@ class Translator extends \Omeka\I18n\Translator
 
         throw new InvalidArgumentException('A message to translate should be stringable.'); // @translate
     }
+
+    public function translatePlural($singular, $plural, $number, $textDomain = 'default', $locale = null)
+    {
+        /**
+         * The process is strange: the singular is generally translated, but not
+         * the plural. So there is a risk of double translation for singular and
+         * a risk of missing translation for plural. And the process implies to
+         * get translations multiple times.
+         *
+         * @see \Laminas\I18n\Translator\Translator::translatePlural()
+         */
+
+        // A quick check for simple strings.
+        if ($singular === $plural) {
+            return $this->translate($singular, $textDomain, $locale);
+        }
+
+        $singularMessage = $singular instanceof Message ? $singular->getMessage() : (string) $singular;
+        $pluralMessage = $plural instanceof Message ? $plural->getMessage() : (string) $plural;
+        $translation = $this->translator->translatePlural($singularMessage, $pluralMessage, (int) $number, $textDomain, $locale);
+
+        if ($translation === $pluralMessage) {
+            if ($plural instanceof PsrMessage) {
+                return $plural->setTranslator($this->translator)->translate($textDomain, $locale);
+            } elseif ($plural instanceof Message && $plural->hasArgs()) {
+                return (string) sprintf($this->translator->translate($plural, $textDomain, $locale), ...$plural->getArgs());
+            } else {
+                return $this->translator->translate($pluralMessage, $textDomain, $locale);
+            }
+        } elseif ($translation === $singularMessage) {
+            if ($singular instanceof PsrMessage) {
+                return $singular->setTranslator($this->translator)->translate($textDomain, $locale);
+            } elseif ($singular instanceof Message && $singular->hasArgs()) {
+                return (string) sprintf($this->translator->translate($singular, $textDomain, $locale), ...$singular->getArgs());
+            } else {
+                return $this->translator->translate($singularMessage, $textDomain, $locale);
+            }
+        } else {
+            return $translation;
+        }
+    }
 }
