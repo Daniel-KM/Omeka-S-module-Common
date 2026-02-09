e49b4656170759776c57942c79fe93238eefa945
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-02-09 00:00:00 +0000

    Cached values for element MediaTypeSelect.

--- src/Service/Form/Element/MediaTypeSelectFactory.php
+++ src/Service/Form/Element/MediaTypeSelectFactory.php
@@ -8,23 +8,30 @@ use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class MediaTypeSelectFactory implements FactoryInterface
 {
+    /**
+     * @var ?array Cache the GROUP BY query result across instances.
+     */
+    protected static $mediaTypes;
+
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        /** @var \Doctrine\DBAL\Connection $connection */
-        $connection = $services->get('Omeka\Connection');
-        $sql = <<<'SQL'
-SELECT `media_type`, `media_type`
-FROM media
-WHERE `media_type` IS NOT NULL
-    AND `media_type` != ""
-GROUP BY `media_type`
-ORDER BY `media_type` ASC;
-SQL;
-        $list = $connection->executeQuery($sql)->fetchAllKeyValue();
+        if (static::$mediaTypes === null) {
+            /** @var \Doctrine\DBAL\Connection $connection */
+            $connection = $services->get('Omeka\Connection');
+            $sql = <<<'SQL'
+                SELECT `media_type`, `media_type`
+                FROM media
+                WHERE `media_type` IS NOT NULL
+                    AND `media_type` != ""
+                GROUP BY `media_type`
+                ORDER BY `media_type` ASC;
+                SQL;
+            static::$mediaTypes = $connection->executeQuery($sql)->fetchAllKeyValue();
+        }
 
         $element = new MediaTypeSelect(null, $options ?? []);
         return $element
-            ->setValueOptions($list)
+            ->setValueOptions(static::$mediaTypes)
             ->setEmptyOption('Select media type…'); // @translate
     }
 }
